d8e0742447fdba3e71468cd7ca58bccf42e55698
JuMalIO <spamjulius@mail.com>
2021-08-01 22:24:22 +0300

    Initial

---
+++ CheapestRrrPartFinder/Downloader.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Web;
+
+namespace CheapestRrrPartFinder
+{
+    public class Downloader
+    {
+        public List<(decimal price, string url)> Download(string url)
+        {
+            var list = new List<(decimal price, string url)>();
+            var html = DownloadHtml(url);
+            var totalPages = ParseTotalPages(html);
+
+            for (var i = 1; i <= totalPages; i++)
+            {
+                if (i != 1)
+                {
+                    html = DownloadHtml(url + $"&page={i}");
+                }
+
+                list.AddRange(ParsePrieceAndUrl(html));
+
+                System.Threading.Thread.Sleep(5000);
+            }
+
+            return list;
+        }
+
+        private static string DownloadHtml(string url)
+        {
+            using (var client = new WebClient())
+            {
+                var htmlCode = client.DownloadString(url);
+                return htmlCode;
+            }
+        }
+
+        private static int ParseTotalPages(string html)
+        {
+            const string searchFor = "span class=\"pages__links\">";
+
+            var i = html.IndexOf(searchFor);
+            var str = html.Substring(i + searchFor.Length, html.Length - i - searchFor.Length);
+            i = str.IndexOf("<");
+            var values = str.Substring(0, i).Split("/");
+
+
+            return int.Parse(values[1].Trim());
+        }
+
+        private static List<(decimal price, string url)> ParsePrieceAndUrl(string html)
+        {
+            int index;
+            var list = new List<(decimal price, string url)>();
+
+            const string searchFor = "<h3><a href=\"https://rrr.lt/autodalis";
+            const string searchFor2 = "Kaina: <strong>";
+
+            while ((index = html.IndexOf(searchFor)) > 0)
+            {
+                html = html.Substring(index + searchFor.Length, html.Length - index - searchFor.Length);
+                index = html.IndexOf("\"");
+
+                string url = $"https://rrr.lt/autodalis{(html.Substring(0, index))}";
+
+                index = html.IndexOf(searchFor2);
+                html = html.Substring(index + searchFor2.Length, html.Length - index - searchFor2.Length);
+                index = html.IndexOf(" ");
+
+                decimal price = decimal.Parse(html.Substring(0, index), CultureInfo.InvariantCulture);
+
+                list.Add((price, url));
+            }
+            return list;
+        }
+    }
+}
---
+++ CheapestRrrPartFinder/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CheapestRrrPartFinder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private List<(decimal price, string url)> Items;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length <= 0)
+            {
+                MessageBox.Show("Error: empty url!");
+                return;
+            }
+
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            Cursor.Current = Cursors.WaitCursor;
+
+            listBox1.Items.Clear();
+
+            Items = new Downloader().Download(textBox1.Text).OrderBy(x => x.price).ToList();
+
+            foreach (var item in Items)
+            {
+                listBox1.Items.Add($"{item.price,20:F2}\t{item.url}");
+            }
+
+            textBox1.Enabled = true;
+            button1.Enabled = true;
+            Cursor.Current = Cursors.Default;
+            SystemSounds.Asterisk.Play();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = ((ListBox)sender).SelectedIndex;
+
+            if (index < Items.Count)
+            {
+                var url = Items[index].url;
+
+                try
+                {
+                    Process.Start(url);
+                }
+                catch
+                {
+                    // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        url = url.Replace("&", "^&");
+                        Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    {
+                        Process.Start("xdg-open", url);
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                    {
+                        Process.Start("open", url);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
